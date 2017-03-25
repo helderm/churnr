e1fedf0d038216ba21ca55aad6846d63735160bc
Helder Martins <helder@spotify.com>
2017-03-25 18:43:38 +0100

    Adding data preprocessing script

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,20 +1,82 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import os
 import click
 import logging
 from dotenv import find_dotenv, load_dotenv
 
+import json
+import pandas as pd
+import glob
+from sklearn import preprocessing
+import numpy as np
 
 @click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
+@click.option('--inpath', default='../../data/raw')
+@click.option('--outpath', default='../../data/processed')
+def main(inpath, outpath):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
     logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+    logger.info('Processing raw data...')
 
+    # load all the csv files
+    with open(os.path.join(inpath, 'meta.json')) as f:
+        meta = json.load(f)
+
+    tablenames = 'user_features_s{}_*'.format(meta['enddate'])
+    usertablename = 'user_ids_sampled_{}'.format(meta['enddate'])
+    fpath = os.path.join(inpath, tablenames)
+    upath = os.path.join(inpath, usertablename)
+    allfiles = glob.glob(fpath)
+
+    logger.info('Loading {} tables from users sampled from {}..'.format(len(allfiles), meta['enddate']))
+
+    # read all feature tables
+    featdf = pd.concat((pd.read_csv(f) for f in allfiles))
+    #df = df.sort_values(by=['time','user_id'])
+    userdf = pd.read_csv(upath)
+
+    df = pd.merge(featdf, userdf, on='user_id', sort=True)
+
+    # delete unnecessessary columns
+    #del df['user_id']
+
+    # normalize
+    features = [f for f in df.columns if f != 'churn' and f != 'time' and f != 'user_id']
+    scaled = preprocessing.scale(df[features])
+    scaled = np.c_[ scaled, df['user_id'], df['time'], df['churn'] ]
+    df = pd.DataFrame(scaled, columns=features + ['user_id', 'time', 'churn'])
+
+    # for lstms, reshape X into timesteps
+    timepoints = df['time'].unique()
+    num_samples = len(df[ df['time'] == timepoints[0] ])
+    features = [f for f in df.columns if f != 'churn' and f != 'time' and f != 'user_id']
+    timesteps = len(timepoints)
+    X = np.empty(shape=(num_samples, timesteps, len(features)))
+    for i, ts in enumerate(timepoints):
+        X[:,i,:] = df[df['time'] == ts][features]
+
+    # extract labels
+    y = df[ df['time'] == df['time'].unique()[0] ]
+    y = y['churn'].values.astype(int)
+
+    # pickle
+    logger.info('Storing normalized dataframe on {}...'.format(outpath))
+    df.to_pickle(os.path.join(outpath, 'user_features_s{}.pkl'.format(meta['enddate'])))
+
+    xpath = os.path.join(outpath, 'user_features_lstm_s{}.npy'.format(meta['enddate']))
+    X.dump(xpath)
+    ypath = os.path.join(outpath, 'user_labels_lstm_s{}.npy'.format(meta['enddate']))
+    y.dump(ypath)
+
+    # write meta file
+    meta = { 'enddate': meta['enddate'], 'x': xpath, 'y': ypath }
+    with open(os.path.join(outpath, 'meta.json'), 'w') as f:
+        json.dump(meta, f)
+
+    logger.info('Done!')
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
