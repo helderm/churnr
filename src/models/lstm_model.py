43901f0152803e6a2d77b28e5e9809b42e392c08
Helder Martins <helder@spotify.com>
2017-03-26 01:33:40 +0100

    Added AUC calculation and plots

--- src/models/lstm_model.py
+++ src/models/lstm_model.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env pythonw
 # -*- coding: utf-8 -*-
 import os
 import click
@@ -14,7 +14,11 @@ from keras.utils.np_utils import to_categorical
 from imblearn.under_sampling import RandomUnderSampler
 from sklearn.model_selection import TimeSeriesSplit
 from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import roc_curve, auc
 import datetime as dt
+
+logger = logging.getLogger('churnr.lstm')
 
 def get_model(timesteps, dim):
     model = Sequential()
@@ -26,7 +30,7 @@ def get_model(timesteps, dim):
 
     model.compile(loss='binary_crossentropy',
                   optimizer='rmsprop',
-                  metrics=['accuracy'])
+                  metrics=['binary_accuracy'])
     return model
 
 
@@ -44,7 +48,28 @@ def get_data(xpath, ypath):
 
     # turn labels into one-hot vectors
     ysc = to_categorical(ys)
-    return Xs, ysc
+
+    # split into train/val and test sets
+    X_tr, X_te, y_tr, y_te = train_test_split(Xs, ysc, test_size=0.2, random_state=42)
+
+    return X_tr, y_tr, X_te, y_te
+
+
+def plot_roc_auc(fpr, tpr, roc_auc, outpath):
+    import matplotlib.pyplot as plt
+    import seaborn
+
+    plt.figure()
+    plt.plot(fpr, tpr, color='darkorange',
+	     lw=1, label='ROC curve (area = %0.2f)' % roc_auc)
+    plt.plot([0, 1], [0, 1], color='navy', lw=1, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver operating characteristic')
+    plt.legend(loc="lower right")
+    plt.savefig(os.path.join(outpath, 'roc.png'), bbox_inches='tight')
 
 
 @click.command()
@@ -54,7 +79,7 @@ def main(inpath, outpath):
     with open(os.path.join(inpath, 'meta.json')) as f:
         meta = json.load(f)
 
-    X, y = get_data(meta['x'], meta['y'])
+    X, y, X_te, y_te = get_data(meta['x'], meta['y'])
 
     # get the model instance
     model = get_model(X.shape[1], X.shape[2])
@@ -63,25 +88,44 @@ def main(inpath, outpath):
         os.makedirs(modeldir)
 
     # set model callbacks
-    chkp_path = os.path.join(modeldir, 'model_e{epoch:02d}-va{val_acc:.2f}-vl{val_loss:.2f}.hdf5')
-    chkp = ModelCheckpoint(chkp_path, monitor='val_acc', save_best_only=True, period=5)
+    chkp_path = os.path.join(modeldir, 'model_e{epoch:02d}-va{val_binary_accuracy:.2f}-vl{val_loss:.2f}.hdf5')
+    chkp = ModelCheckpoint(chkp_path, monitor='val_binary_accuracy', save_best_only=True, period=5)
     reducelr = ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=3, min_lr=0.001, verbose=1)
     tensorboard = TensorBoard(log_dir=os.path.join(modeldir, 'logs'), write_images=True)
     callbacks = [chkp, reducelr, tensorboard]
 
     # split data into train and val sets
     tscv = TimeSeriesSplit(n_splits=3)
-    for train_idx, test_idx in tscv.split(X):
+
+    # train the model for each train / val folds
+    for train_idx, val_idx in tscv.split(X):
         X_train = X[train_idx,:,:]
-        X_test = X[test_idx,:,:]
+        X_val = X[val_idx,:,:]
 
         y_train = y[train_idx,:]
-        y_test = y[test_idx,:]
+        y_val = y[val_idx,:]
 
         model.fit(X_train, y_train,
-                  batch_size=64, epochs=20,
-                  validation_data=(X_test, y_test),
+                  batch_size=64, epochs=10,
+                  validation_data=(X_val, y_val),
                   callbacks=callbacks)
+
+    # print the model test metrics
+    metrics = model.evaluate(X_te, y_te, verbose=0)
+    metrics_names = model.metrics_names if type(model.metrics_names) == list else [model.metrics_names]
+    if type(metrics) != list:
+        metrics = [metrics]
+    logger.info('** Test metrics **')
+    for metric, metric_name in zip(metrics, metrics_names):
+        logger.info('-- {0}: {1:.3f}'.format(metric_name, metric))
+
+    # calculate roc and auc
+    y_pred = model.predict(X_te)
+    fpr, tpr, _ = roc_curve(y_te[:,1], y_pred[:,1])
+    roc_auc = auc(fpr, tpr)
+    logger.info('-- auc: {0:.3f}'.format(roc_auc))
+
+    plot_roc_auc(fpr, tpr, roc_auc, modeldir)
 
     with open(os.path.join(modeldir, 'config.json'), 'w') as f:
         json.dump(model.get_config(), f)
