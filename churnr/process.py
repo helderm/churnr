ea2b8a628177738ae41c723de4cddbf7d3c763f5
Helder Martins <helder@spotify.com>
2017-05-17 14:30:12 +0200

    Reworked the process stage, now doing it all on BigQuery and avoiding loading the dataset into memory

--- churnr/process.py
+++ churnr/process.py
@@ -1,158 +1,364 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import os
-import argparse
 import logging
-import joblib
-import json
+import uuid
+import argparse
+from google.cloud import bigquery as bq
+import google.cloud.storage as gcs
 import datetime as dt
-import pandas as pd
-import glob
-from sklearn import preprocessing
-from sklearn.utils import shuffle
-from sklearn.model_selection import train_test_split
-import numpy as np
+import time
+import json
 
-import churnr.utils as utils
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger('churnr.process')
 
+FEATURES = ['skip_ratio', 'secs_played']
+FEATURES_IAT = ['iat'] # intertimestep features
+FEATURES_SUM = ['sum_secs_played']
+FEATURES_COUNT = ['total_streams']
+
+FEATURES_ALL = FEATURES + FEATURES_IAT + FEATURES_SUM + FEATURES_COUNT
 
 def main(exppath, experiment, dsname):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger.info('Initializing data preprocessing...')
+    """ Process the data in a format suitable for training """
+    logger.info('Starting data processing for dataset {}...'.format(dsname))
 
-    # load all the csv files
-    with open(exppath) as fi:
-        dsconf = json.load(fi)[experiment]['datasets']
+    with open(exppath) as f:
+        exp = json.load(f)
 
     # load experiment configuration
-    keys = ['rawpath', 'procpath', 'testsize', 'gsoutput', 'enddate', 'project', 'obsdays', 'preddays', 'timesplits']
+    keys = ['project', 'dataset', 'enddate', 'timesplits', 'gsoutput', 'testsize', 'retainedshare']
     conf = {}
     for key in keys:
-        conf[key] = dsconf[dsname][key] if key in dsconf[dsname] else dsconf['global'][key]
+        conf[key] = exp[experiment]['datasets'][dsname][key] if key in exp[experiment]['datasets'][dsname] else exp[experiment]['datasets']['global'][key]
     if conf['enddate'] == 'yesterday':
         conf['enddate'] = (dt.datetime.today() - dt.timedelta(days=1)).strftime('%Y%m%d')
     conf['experiment'] = experiment
     conf['dsname'] = dsname
 
-    rawpath = conf['rawpath']
-    procpath = conf['procpath']
-    testsize = conf['testsize']
-    gsoutput = conf['gsoutput']
-    project = conf['project']
+    gspath = os.path.join(conf['gsoutput'], experiment, dsname)
 
-    inpath = os.path.join(rawpath, experiment, dsname)
+    try:
+        client = bq.Client(project=conf['project'])
 
-    # if raw path is empty, assume the data is in GCS and try to download it
-    if not os.path.exists(inpath):
-        logger.info('Raw data not found in {}, trying to download it from GCS...'.format(inpath))
-        gspath = os.path.join(gsoutput, experiment, dsname)
-        tablenames = utils.get_table_names(conf)
-        utils.extract_dataset_to_disk(inpath, tablenames, project, gspath)
+        # create dataset
+        ds = client.dataset(conf['dataset'])
+        if not ds.exists():
+            ds.location = 'EU'
+            ds.create()
 
-    # read all csv tables
-    tablenames = 'features_*'
-    usertablename = 'users_*'
-    fpath = os.path.join(inpath, tablenames)
-    upath = os.path.join(inpath, usertablename)
-    allfiles = glob.glob(fpath)
+        features_table = ds.table(name='features_{}_{}_{}'.format(conf['experiment'], conf['dsname'], conf['enddate']))
+        users_table = ds.table(name='users_{}_{}_{}'.format(conf['experiment'], conf['dsname'], conf['enddate']))
 
-    logger.info('Loading {} tables from {}..'.format(len(allfiles), inpath))
+        # normalize feature values
+        features_table, jobs = normalize_features(features_table, ds, client, conf)
+        wait_for_jobs(jobs)
 
-    dtype = {'consumption_time': float, 'session_length': float, 'skip_ratio': float,
-                'unique_pages': float, 'user_id': str, 'time': int,
-                'consumption_time_std': float, 'session_length_std': float, 'skip_ratio_std': float,
-                'unique_pages_std': float, 'backfill': bool}
+        # aggregate the rows into one per user with all its timesteps
+        features_table, jobs = aggregate_features(features_table, users_table, ds, client, conf)
+        wait_for_jobs(jobs)
 
-    featdf = pd.concat((pd.read_csv(f, dtype=dtype) for f in allfiles))
-    userdf = pd.concat((pd.read_csv(f) for f in glob.glob(upath)))
+        # split into train, val and test sets
+        train_features_table, test_features_table, val_features_table, jobs = train_test_val_split(features_table, ds, client, conf)
+        wait_for_jobs(jobs)
 
-    logger.info('Merging tables into a single dataframe...')
-    df = pd.merge(featdf, userdf, on='user_id', sort=True)
+        # undersample the training set
+        train_us_features_table, jobs = undersample_features(train_features_table, ds, client, conf)
+        wait_for_jobs(jobs)
 
-    nobf_df = df[df.backfill == False]
+        # export to gcs
+        tables = [train_features_table, train_us_features_table, test_features_table, val_features_table]
+        jobs = dump_features_to_gcs(tables, gspath, conf['project'], client)
+        wait_for_jobs(jobs)
 
-    # normalize
+        logger.info('Data processing of dataset {} done!'.format(dsname))
+
+    except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+def normalize_features(ftable, ds, client, conf):
+    """ Normalize the feature tables with a zero mean and unit variance """
     logger.info('Normalizing features...')
-    features = [f for f in df.columns if f != 'churn' and f != 'time' and f != 'user_id' and f != 'backfill']
-    scaled = preprocessing.scale(nobf_df[features], copy=False)
-    scaled = np.c_[ scaled, nobf_df['user_id'], nobf_df['time'], nobf_df['churn'], nobf_df['backfill'] ]
-    new_df = pd.DataFrame(scaled, columns=features + ['user_id', 'time', 'churn', 'backfill'], )
-    for f in features:
-        new_df[f] = new_df[f].astype(np.float)
-    new_df['time'] = new_df['time'].astype(np.int)
 
-    # join the normalized data with the ones backfilled
-    df = pd.concat([new_df, df[df.backfill == True]])
-    df = df.sort_values(['user_id','time'])
+    select_with = ''
+    select = ''
+    select_union = ''
+    for f in FEATURES_ALL:
+        select_with += 'AVG({feat}) AS {feat}_avg, STDDEV_POP({feat}) AS {feat}_stddev,'.format(feat=f)
+        select += '( ({feat} - {feat}_avg ) / {feat}_stddev ) AS {feat},'.format(feat=f)
+        select_union += '{feat},'.format(feat=f if f != 'sum_secs_played' else '( ({feat} - (SELECT {feat}_avg FROM avgs_stdvs)) / (SELECT {feat}_stddev FROM avgs_stdvs) ) AS {feat}'.format(feat=f))
 
-    # for lstms, reshape X into timesteps
-    logger.info('Generating sequential dataset...')
-    timepoints = df['time'].unique()
-    #num_samples = len(df[ df['time'] == timepoints[0] ])
-    num_samples = len(df['user_id'].unique())
-    #features = [f for f in df.columns if f != 'churn' and f != 'time' and f != 'user_id']
-    timesteps = len(timepoints)
-    X = np.empty(shape=(num_samples, timesteps, len(features)))
-    for i, ts in enumerate(timepoints):
-        X[:,i,:] = df[df['time'] == ts][features]
+    nftable = ds.table(name='features_{}_{}_n_{}'.format(conf['experiment'], conf['dsname'], conf['enddate']))
+    if nftable.exists():
+        nftable.delete()
 
-    # for aggregated models, get the mean of features
-    logger.info('Generating aggregated dataset...')
-    num_samples = len(nobf_df['user_id'].unique())
-    X_agg = np.empty(shape=(num_samples, len(features)))
-    X_agg[:,:] = nobf_df.groupby('user_id', sort=True)[features].mean()
+    query = """\
+     WITH
+       avgs_stdvs AS (
+       SELECT {select_with} 0
+       FROM
+         `{project}.{dataset}.{table}`
+       WHERE
+         backfill = FALSE )
+     SELECT
+       {select}
+       user_id,
+       time
+     FROM
+       `{project}.{dataset}.{table}`,
+       avgs_stdvs
+     WHERE
+         backfill = FALSE
+     UNION ALL
+     SELECT
+         {select_union}
+        user_id,
+        time
+     FROM
+       `{project}.{dataset}.{table}`,
+       avgs_stdvs
+     WHERE
+         backfill = TRUE
+    """.format(select_with=select_with, select=select, select_union=select_union, table=ftable.name, project=conf['project'], dataset=ds.name)
 
-    # extract labels
-    logger.info('Extracting labels...')
-    y = df[ df['time'] == df['time'].unique()[0] ]
-    y = y['churn'].values.astype(int)
+    jobname = 'features_norm_job_' + str(uuid.uuid4())
+    job = client.run_async_query(jobname, query)
+    job.destination = nftable
+    job.allow_large_results = True
+    job.write_disposition = 'WRITE_TRUNCATE'
+    job.use_legacy_sql = False
+    job.begin()
 
-    # shuffle on the samples dimension
-    logger.info('Shuffling...')
-    X, X_agg, y = shuffle(X, X_agg, y)
+    return nftable, [job]
 
-    # split into train and test sets
-    logger.info('Splitting into train and test sets...')
-    idxs = np.arange(X_agg.shape[0])
-    _, _, _, _, idxs_tr, idxs_te = train_test_split(X_agg, y, idxs, test_size=testsize, random_state=42)
-    X_tr = X[idxs_tr,:,:]
-    X_agg_tr = X_agg[idxs_tr,:]
-    y_tr = y[idxs_tr]
-    X_te = X[idxs_te,:,:]
-    X_agg_te = X_agg[idxs_te,:]
-    y_te = y[idxs_te]
 
-    # create output data dir
-    outdir = os.path.join(procpath, experiment, dsname)
-    if os.path.exists(outdir):
-        import shutil
-        shutil.rmtree(outdir)
-    os.makedirs(outdir)
+def aggregate_features(ftable, utable, ds, client, conf):
+    """ """
+    logger.info('Aggregating features...')
 
-    # pickle
-    logger.info('Storing matrices on {}...'.format(outdir))
-    #df.to_csv(os.path.join(outdir, 'user_features_full.gz'), compression='gzip')
+    agftable = ds.table(name='features_{}_{}_a_{}'.format(conf['experiment'], conf['dsname'], conf['enddate']))
+    if agftable.exists():
+        agftable.delete()
 
-    xpath = os.path.join(outdir, 'features_seq_train.gz')
-    joblib.dump(X_tr, xpath)
-    xpath = os.path.join(outdir,  'features_agg_train.gz')
-    joblib.dump(X_agg_tr, xpath)
-    xpath = os.path.join(outdir, 'features_seq_test.gz')
-    joblib.dump(X_te, xpath)
-    xpath = os.path.join(outdir,  'features_agg_test.gz')
-    joblib.dump(X_agg_te, xpath)
+    select = ''
+    for f in FEATURES_ALL:
+        select += 'ARRAY_AGG(n.{feat}) as {feat},'.format(feat=f)
 
-    ypath = os.path.join(outdir,  'labels_train.gz')
-    joblib.dump(y_tr, ypath)
-    ypath = os.path.join(outdir,  'labels_test.gz')
-    joblib.dump(y_te, ypath)
+    query = """
+        SELECT
+            {select}
+            ARRAY_AGG(n.time) as times,
+            ANY_VALUE(u.churn) as churn,
+            u.user_id
+        FROM
+            `{project}.{dataset}.{ftable}` as n
+        JOIN
+            `{project}.{dataset}.{utable}` as u
+        ON n.user_id = u.user_id
+        GROUP BY
+            u.user_id
+    """.format(project=conf['project'], dataset=ds.name, ftable=ftable.name,
+                utable=utable.name, select=select)
 
-    logger.info('Done!')
+    jobname = 'features_agg_job_' + str(uuid.uuid4())
+    job = client.run_async_query(jobname, query)
+    job.destination = agftable
+    job.allow_large_results = True
+    job.write_disposition = 'WRITE_TRUNCATE'
+    job.use_legacy_sql = False
+    job.begin()
+
+    return agftable, [job]
+
+
+def train_test_val_split(ftable, ds, client, conf):
+    """ """
+    logger.info('Splitting features into train, val and test sets...')
+
+    share = conf['testsize']
+    jobs = []
+
+    testft = ds.table(name='features_{}_{}_te_{}'.format(conf['experiment'], conf['dsname'], conf['enddate']))
+    if testft.exists():
+        testft.delete()
+
+    query = """
+        SELECT
+            *
+        FROM
+            `{project}.{dataset}.{ftable}`
+        WHERE
+           MOD(ABS(FARM_FINGERPRINT(SHA1(user_id))), {share}) = 0
+    """.format(project=conf['project'], dataset=ds.name, ftable=ftable.name,
+                share=int(1/share))
+
+    jobname = 'features_split_job_' + str(uuid.uuid4())
+    job = client.run_async_query(jobname, query)
+    job.destination = testft
+    job.allow_large_results = True
+    job.write_disposition = 'WRITE_TRUNCATE'
+    job.use_legacy_sql = False
+    job.begin()
+
+    jobs.append(job)
+
+    valft = ds.table(name='features_{}_{}_val_{}'.format(conf['experiment'], conf['dsname'], conf['enddate']))
+    if valft.exists():
+        valft.delete()
+
+    query = """
+        SELECT
+            *
+        FROM
+            `{project}.{dataset}.{ftable}`
+        WHERE
+           MOD(ABS(FARM_FINGERPRINT(SHA1(user_id))), {share}) = 1
+    """.format(project=conf['project'], dataset=ds.name, ftable=ftable.name,
+                share=int(1/share))
+
+    jobname = 'features_split_job_' + str(uuid.uuid4())
+    job = client.run_async_query(jobname, query)
+    job.destination = valft
+    job.allow_large_results = True
+    job.write_disposition = 'WRITE_TRUNCATE'
+    job.use_legacy_sql = False
+    job.begin()
+
+    jobs.append(job)
+
+    trft = ds.table(name='features_{}_{}_tr_{}'.format(conf['experiment'], conf['dsname'], conf['enddate']))
+    if trft.exists():
+        trft.delete()
+
+    query = """
+        SELECT
+            *
+        FROM
+            `{project}.{dataset}.{ftable}`
+        WHERE
+           MOD(ABS(FARM_FINGERPRINT(SHA1(user_id))), {share}) NOT IN (0,1)
+    """.format(project=conf['project'], dataset=ds.name, ftable=ftable.name,
+                share=int(1/share))
+
+    jobname = 'features_split_job_' + str(uuid.uuid4())
+    job = client.run_async_query(jobname, query)
+    job.destination = trft
+    job.allow_large_results = True
+    job.write_disposition = 'WRITE_TRUNCATE'
+    job.use_legacy_sql = False
+    job.begin()
+
+    jobs.append(job)
+
+    return trft, testft, valft, jobs
+
+
+def undersample_features(ftable, ds, client, conf):
+    """ """
+    logger.info('Undersampling...')
+    share = conf['retainedshare']
+
+    usft = ds.table(name='features_{}_{}_trus_{}'.format(conf['experiment'], conf['dsname'], conf['enddate']))
+    if usft.exists():
+        usft.delete()
+
+    query = """
+        SELECT
+          *
+        FROM
+          `{project}.{dataset}.{table}`
+        WHERE
+          MOD(ABS(FARM_FINGERPRINT(MD5(user_id))), CAST(1/(
+              SELECT
+                SUM(churn) / COUNT(churn)
+              FROM
+                `{project}.{dataset}.{table}`) AS INT64)) < {share}
+          AND churn = 0
+        UNION ALL
+        SELECT
+          *
+        FROM
+          `{project}.{dataset}.{table}`
+        WHERE
+          churn = 1
+    """.format(project=conf['project'], dataset=ds.name, table=ftable.name,
+                share=share)
+
+    jobname = 'features_undersample_job_' + str(uuid.uuid4())
+    job = client.run_async_query(jobname, query)
+    job.destination = usft
+    job.allow_large_results = True
+    job.write_disposition = 'WRITE_TRUNCATE'
+    job.use_legacy_sql = False
+    job.begin()
+
+    return usft, [job]
+
+
+def dump_features_to_gcs(ft_tables, dest, project, client):
+    """ Dump generated tables as files on Google Cloud Storage"""
+    logger.info('Dumping {} tables to {}...'.format(len(ft_tables), dest))
+
+    gs_client = gcs.Client(project)
+    split_uri = dest.split('/')
+    filepath = '/'.join(split_uri[3:])
+    bucket_name = split_uri[2]
+    bucket = gcs.Bucket(gs_client, bucket_name)
+
+    jobs = []
+    for ft_table in ft_tables:
+        filename_shard = ft_table.name + '{0:012d}'.format(0)
+        blob = gcs.Blob(name=os.path.join(filepath, filename_shard), bucket=bucket)
+        if blob.exists():
+            count = 0
+            while blob.exists():
+                logger.info(' -- Removing blob {}'.format(blob.path))
+                blob.delete()
+
+                count += 1
+                filename_shard = ft_table.name + '{0:012d}'.format(count)
+                blob = gcs.Blob(name=os.path.join(filepath, filename_shard), bucket=bucket)
+
+        path = dest + '/' + ft_table.name + '*'
+        jobname = 'features_dump_job_' + str(uuid.uuid4())
+        job = client.extract_table_to_storage(jobname, ft_table, path)
+        job.destination_format = 'NEWLINE_DELIMITED_JSON'
+        job.begin()
+
+        jobs.append(job)
+
+    return jobs
+
+
+def wait_for_jobs(jobs):
+    """ wait for async GCP jobs to finish """
+    logger.info("Waiting for {} jobs to finish...".format(len(jobs)))
+
+    cntr = 0
+    for job in jobs:
+        if cntr % 10 == 0:
+            logger.info('{} out of {} jobs finished...'.format(cntr, len(jobs)))
+        cntr += 1
+
+        numtries = 9000
+        job.reload()
+        while job.state != 'DONE':
+            time.sleep(10)
+            job.reload()
+            numtries -= 1
+
+            if numtries == 0:
+                logger.error('Job {} timed out! Aborting!'.format(job.name))
+                raise Exception('Async job timed out')
+
+        if job.errors:
+            logger.error('Job {} failed! Aborting!'.format(job.name))
+            raise Exception('Async job failed')
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Data processer')
@@ -163,3 +369,5 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     main(exppath=args.exppath, experiment=args.experiment, dsname=args.dsname)
+
+
