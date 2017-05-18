5b6bf6431b80201b83b4e47c3c88b246bb71a655
Helder Martins <helder@spotify.com>
2017-05-18 13:21:51 +0200

    Changed app args, forced a gc after experiment

--- churnr/app.py
+++ churnr/app.py
@@ -4,28 +4,30 @@ import os
 import argparse
 import logging
 import json
+import gc
 
 import matplotlib
 matplotlib.use('Agg')
 
+import churnr
 from churnr import extract, process, train, plot
 
 logger = logging.getLogger('churnr.experiment')
 
 
-def run(exppath, experiment, stage, singlestage, debug, hddump):
+def run(args):
     logger.info('Initializing experiment...')
 
-    with open(exppath) as fi:
-        expconf = json.load(fi)[experiment]
+    with open(args.exppath) as fi:
+        expconf = json.load(fi)[args.experiment]
 
     datasets = expconf['datasets']
     models = expconf['models']
     plots = expconf['plots']
-    expabspath = os.path.abspath(exppath)
+    expabspath = os.path.abspath(args.exppath)
 
     # extract stage
-    if stage in ['extract']:
+    if 'extract' in args.stages:
         # sort the datasets by size of observation or prediction window, and sample users only on
         # the largest one as to have the same set of user ids of across all datasets
         if 'preddays' not in datasets['global']:
@@ -37,25 +39,25 @@ def run(exppath, experiment, stage, singlestage, debug, hddump):
             if dsname == 'global':
                 continue
 
-            extract.main(exppath=expabspath, experiment=experiment, dsname=dsname, hddump=hddump, sampleusers=sampleusers)
+            extract.main(exppath=expabspath, experiment=args.experiment, dsname=dsname, hddump=False, sampleusers=sampleusers)
             sampleusers = False
 
     # process stage
-    if stage in ['extract', 'process'] and (not singlestage or (singlestage and stage == 'process')):
+    if 'process' in args.stages:
         for dsname in datasets.keys():
             if dsname == 'global':
                 continue
 
-            process.main(exppath=expabspath, experiment=experiment, dsname=dsname)
+            process.main(exppath=expabspath, experiment=args.experiment, dsname=dsname)
 
     # train stage
-    if stage in ['extract', 'process', 'train'] and (not singlestage or (singlestage and stage == 'train')):
+    if 'train' in args.stages:
         for dsname in datasets.keys():
             if dsname == 'global':
                 continue
 
             # remove any local files stored in another run
-            dspath = os.path.join(datasets['global']['procpath'], experiment, dsname)
+            dspath = os.path.join(datasets['global']['procpath'], args.experiment, dsname)
             if os.path.exists(dspath):
                 import shutil
                 shutil.rmtree(dspath)
@@ -65,31 +67,35 @@ def run(exppath, experiment, stage, singlestage, debug, hddump):
                 if modelname == 'global':
                     continue
 
-                train.main(exppath=expabspath, experiment=experiment, dsname=dsname, modelname=modelname, debug=debug)
+                train.main(exppath=expabspath, experiment=args.experiment, dsname=dsname, modelname=modelname, debug=args.debug)
+
+                # dispatches garbage collector after each training
+                gc.collect()
 
     # plot stage
-    if stage in ['extract', 'process', 'train', 'plot'] and (not singlestage or (singlestage and stage == 'plot')):
+    if 'plot' in args.tages:
         for plotname in plots.keys():
             if plotname == 'global':
                 continue
 
-            plot.main(exppath=expabspath, experiment=experiment, plotname=plotname)
+            plot.main(exppath=expabspath, experiment=args.experiment, plotname=plotname)
 
     logger.info('Done!')
 
 
 def main():
+    path = os.path.dirname(churnr.__file__)
+
     parser = argparse.ArgumentParser(description='Experiment dispatcher')
-    parser.add_argument('--exppath', default='./experiments.json', help='Path to the experiments json file')
+    parser.add_argument('--exppath', default=os.path.join(path, 'experiments.json'), help='Path to the experiments json file')
     parser.add_argument('--experiment', default='temporal_static', help='Name of the experiment being performed')
-    parser.add_argument('--stage', default='extract', help='Stage that the experiment will start from', choices=['extract', 'process', 'train', 'plot'])
-    parser.add_argument('--singlestage', default=False, help='Stage that the experiment will start from', action='store_true')
+    parser.add_argument('--stages', default=['extract','process','train', 'plot'], help='Stages that will be executed', nargs='*')
     parser.add_argument('--hddump', default=False, help='Store files in local disk', action='store_true')
     parser.add_argument('--debug', default=False, help='Debug flag that sped up some stages', action='store_true')
 
     args = parser.parse_args()
 
-    run(exppath=args.exppath, experiment=args.experiment, stage=args.stage, singlestage=args.singlestage, debug=args.debug, hddump=args.hddump)
+    run(args)
 
 if __name__ == '__main__':
     main()
