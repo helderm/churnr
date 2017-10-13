a23c7670852d570ab1251118f4fd21465afef719
Helder Martins <heldergaray@gmail.com>
2017-10-13 21:45:54 +0200

    Updated with final comments

--- churnr/scala/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-RELPATH="`dirname \"$0\"`"
-ABSPATH="`( cd \"$RELPATH\" && pwd )`"
-cd $ABSPATH; sbt -Dbigquery.project=$1 "runMain com.spotify.churnr.Parser \
---project=$1 --runner=DataflowRunner \
---zone=europe-west1-d --output=$2"; cd --
